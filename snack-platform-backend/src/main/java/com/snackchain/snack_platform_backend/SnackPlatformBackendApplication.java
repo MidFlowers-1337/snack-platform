c31e15578eece981ffa18ea2aad6c8f154dea462
MidFlowers <1537465221z@gmail.com>
2025-12-13 16:17:42 +0800

    first commit

---
+++ snack-platform-backend/src/main/java/com/snackchain/snack_platform_backend/SnackPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.snackchain.snack_platform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackPlatformBackendApplication.class, args);
+	}
+
+}
